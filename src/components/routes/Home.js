3236dd18466318ff578261636986da6aa8cec702
Stefanescu Mihai <Mihai.Stefanescu@msg.group>
2022-08-20 15:48:21 +0300

    Added Search API connection and search bar

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import MainPageLayout from '../layouts/MainPageLayout';
 
 const Home = () => {
-    return ( <MainPageLayout>Homepage here</MainPageLayout> );
+    const [input, setInput] = useState('');
+
+    const onInputChange = (event) => {
+        setInput(event.target.value)
+    }
+
+    const onSearch = () => {
+        // https://api.tvmaze.com/search/shows?q=girls
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+        .then(response => response.json())
+        .then(resultJson => console.log(resultJson))
+    }
+
+    const onKeyDown = (event) => {
+        if(event.keyCode === 13){
+            onSearch(input)
+        }
+    }
+
+    return ( 
+        <MainPageLayout>
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+            <button type='button' onClick={onSearch}>Search</button>
+        </MainPageLayout>
+    );
 }
  
 export default Home;
