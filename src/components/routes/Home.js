d6b6a5c916bb70a7c1f9ff691fad4fad6f024d96
Stefanescu Mihai <Mihai.Stefanescu@msg.group>
2022-08-20 16:04:05 +0300

    Added search results display  and encapsulated api

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
+import { apiGet } from '../../api/tv-maze-api';
 import MainPageLayout from '../layouts/MainPageLayout';
 
 const Home = () => {
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
 
     const onInputChange = (event) => {
         setInput(event.target.value)
     }
 
     const onSearch = () => {
-        // https://api.tvmaze.com/search/shows?q=girls
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-        .then(response => response.json())
-        .then(resultJson => console.log(resultJson))
+        apiGet(`/search/shows?q=${input}`)
+         .then(results => setResults(results));
     }
 
     const onKeyDown = (event) => {
@@ -22,10 +22,34 @@ const Home = () => {
         }
     }
 
+    const renderResults = () => {
+        if(results && results.length === 0){
+            return <div>No results</div>
+        }
+
+        if(results && results.length > 0){
+            return (
+                <div>
+                    {results.map(item => <div key={item.show.id}>{item.show.name}</div>)}
+                </div>
+            );
+        }
+
+    }
+
     return ( 
         <MainPageLayout>
-            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
-            <button type='button' onClick={onSearch}>Search</button>
+            <input 
+                type="text" 
+                onChange={onInputChange} 
+                onKeyDown={onKeyDown} 
+                value={input}
+            />
+            <button type='button' onClick={onSearch}>
+                Search
+            </button>
+            {renderResults()}
+
         </MainPageLayout>
     );
 }
